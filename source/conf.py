e955dea209f4363e7ec20035c4b9186263a54540
Dmitry Baryshnikov <bishop.dev@gmail.com>
2016-09-13 17:24:13 +0300

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -62,7 +62,7 @@ author = u'Команда NextGIS'
 # built documents.
 #
 # The short X.Y version.
-version = '2.4.0'
+version = '2.4.1'
 # The full version, including alpha/beta/rc tags.
 release = '2.4'
 
