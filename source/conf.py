58aeae709e400b3d623d6e7ed616342e33b045c4
PraxisNFP <praxisnfp@gmail.com>
2016-12-19 18:24:59 +0530

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -77,7 +77,8 @@ language = 'en'
 # non-false value, then it is used:
 #today = ''
 # Else, today_fmt is used as the format for a strftime call.
-today_fmt = '%d-%m-%Y'
+#today_fmt = '%d-%m-%Y'
+today_fmt = '%d %B %Y'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -162,7 +163,7 @@ html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
+#html_last_updated_fmt = '%d - %m - %Y'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
@@ -229,7 +230,7 @@ latex_elements = {
 
 'classoptions': ',openany,oneside', 
 
-'babel' : '\\usepackage[russian]{babel}',
+'babel' : '\\usepackage{babel}',
 
 'fontpkg': '\\usepackage{dejavu}',
 # Additional stuff for the LaTeX preamble.
